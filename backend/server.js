60ee786410a8341adde945178381a564204460cc
Aniket Chugh <oneaniket13@gmail.com>
2026-03-15 21:25:38 +0530

    done with modular design and getting functional and non-functional requirements

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 import express from "express"
-import authRouter from "./src/routes/auth.route.js";
-import messageRouter from "./src/routes/messages.route.js";
+
 const app = express();
 
 
@@ -8,8 +7,7 @@ app.get("/", (req, res) => {
     res.send("everything going good mf")
 })
 
-app.use("/api/auth", authRouter);
-app.use("/api/messages", messageRouter);
+
 
 app.listen(5000, () => {
     console.log("server is running with no error");
--- backend/src/controllers/auth.controller.js
+++
@@ -1,4 +0,0 @@
-export const signup = (req, res) => {
-    const { username, account_name, email, user_pass } = req.query;
-    console.log(username, account_name, email, user_pass);
-} 
